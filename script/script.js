4bc98c15b1b9967c33afa0fb5fc7a610497e687a
LorenzoRu <80577134+LorenzoRu@users.noreply.github.com>
2021-03-19 22:15:15 +0100

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,7 @@
+const burgerContainer = document.querySelector('.burger-container');
+const navLinks = document.querySelector('.nav-links');
+
+burgerContainer.addEventListener('click', () => {
+    burgerContainer.classList.toggle('active');
+    navLinks.classList.toggle('active');
+})
